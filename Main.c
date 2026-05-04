b3ad315f43a64a39706483fc7ade745d87d376ee
jaocSec <jaoc2001@hotmail.com>
2026-05-04 02:18:22 -0300

    fix: Corregido lógica de guardado de los puntos de interés

--- Main.c
+++ Main.c
@@ -63,8 +63,8 @@ int main(int argc, char const *argv[])
 
         char buffer[256];
 
-        Calle lista_calles[100]; //Lista de structs para guardar calles
-        PuntosInteres lista_puntos[100]; //Lista de structs para guardar puntos de interés
+        Calle lista_calles[50]; //Lista de structs para guardar calles
+        PuntosInteres lista_puntos[50]; //Lista de structs para guardar puntos de interés
 
         int num_calles= 0, num_puntos= 0; //Variables para guardar la cantidad de lineas que vienen después
         if(fgets(buffer, 256, file))
@@ -82,12 +82,43 @@ int main(int argc, char const *argv[])
         {
             sscanf(buffer, "%d", &num_puntos); //guardar numero de lineas de puntos
         }
-
-        for(int i= 0; i < num_puntos; i++){ //ciclo que guarda cada dato de la linea en la lista de structs de puntos
+        for (int i= 0; i < num_puntos; i++)
             if(fgets(buffer, 256, file)){
-                sscanf(buffer, "%s %s %lf %lf", lista_puntos[i].nombre_punto, lista_puntos[i].nombre_calle, &lista_puntos[i].x, &lista_puntos[i].y);
+                double pos_leida;
+
+                sscanf(buffer, "%s %s %lf", lista_puntos[i].nombre_punto, lista_puntos[i].nombre_calle, &pos_leida);
+
+                for(int j= 0; j < num_calles; j++){
+                    if (strcmp(lista_puntos[i].nombre_calle, lista_calles[j].nombre) == 0)
+
+                        if (lista_calles[j].sentido == 'X') // Si el sentido X, la coordenada x será la posición dada por el archivo
+                        {
+                            lista_puntos[i].x = pos_leida;
+                        }
+                            if (lista_calles[j].y1 == lista_calles[j].y2){ //Si es horizontal, y será constante (la de la calle)
+                                lista_puntos[i].y = lista_calles[j].y1;
+
+                            } else{ // Si es diagonal, se calcula la pendiente para usar la formula punto-pendiente
+
+                                double m= (lista_calles[j].y2 - lista_calles[j].y1)/(lista_calles[j].x2 - lista_calles[j].x1);
+                                lista_puntos[i].y= lista_calles[j].y1 + m *(pos_leida - lista_calles[j].x1);
+                            }
+
+
+                        if (lista_calles[j].sentido == 'Y') // Si el sentido Y, la coordenada y será la posición dada por el archivo
+                        {
+                            lista_puntos[i].x = pos_leida;
+                        }
+                            if (lista_calles[j].x1 == lista_calles[j].x2){ //Si es horizontal, x será constante (la de la calle)
+                                lista_puntos[i].x = lista_calles[j].x1;
+
+                            } else{ // Si es diagonal, se calcula la pendiente para usar la formula punto-pendiente
+
+                                double m= (lista_calles[j].y2 - lista_calles[j].y1)/(lista_calles[j].x2 - lista_calles[j].x1);
+                                lista_puntos[i].x= lista_calles[j].x1 + m *(pos_leida - lista_calles[j].y1);
+                            }
+                }
             }
-        }
 
         fclose(file);
         printf("Mapa cargado!\n");
